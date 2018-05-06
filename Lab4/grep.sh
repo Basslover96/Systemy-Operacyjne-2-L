b2fe518a92a596a8837841c2f5b2667a0a0347d6
Basslover <dobrysiewiczk@interia.pl>
2018-05-07 01:10:31 +0200

    Przygotowanie do laboratorium nr 4

---
+++ Lab4/grep.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+if [ $# -ne 1 ]
+then
+	echo "Nieprawidłowa liczba argumentów!"
+	exit 1
+else
+	if [ ! -d $1 ]
+	then
+       		echo "$1 nie jest poprawną ścieżką do katalogu!"
+		exit 1
+	else
+		obecny_katalog=$pwd
+		cd $(realpath $1)
+		echo "Ilość linii zawierających słówko TODO:"
+		grep -r "TODO" * | wc -l
+		echo "Ilość pustych linii:"
+		grep -r -v '\S' * | wc -l
+		echo "Ilość niepustych linii:"
+		grep -r '\S' * | wc -l
+		echo "Ilość linii zakończonych średnikiem:"
+		grep -r -E ".*;" * | wc -l
+		echo "Ilość linii zawierających tylko klamrę { lub }:"
+		grep -r -E '^\{$|^\}$' * | wc -l
+		echo "Ilość linii bez liter a-z i A-Z:"
+		grep -r -v -E '[a-z]|[A-Z]' * | wc -l
+		echo "Ilość linii zawierających dwa otwierające nawiasy obok siebie:"
+		grep -r -E ".*\(\(.*" * | wc -l
+		echo "Ilość linii zawierających liczby heksadecymalne:"
+ 		grep -r -E '0x[0-9a-fA-F]+' * | wc -l 
+		echo "Ilość linii zakończonych trójliterowym rozszerzeniem pliku:"
+		grep -r -E '.+\.[a-zA-Z0-9]{3}$' * | wc -l
+		echo "Ilość struktur:"
+		grep -r -E '^struct .+ \{$' * | wc -l
+		cd $obecny_katalog
+	fi
+fi
